fa890ae3b722b1e9d0c1f14a38dfc20cdb99c091
leshka <ru.a.e@ya.ru>
2019-02-09 14:04:44 +0300

    0902

--- Mig/backup.bat
+++ Mig/backup.bat
@@ -6,8 +6,7 @@ set s=%TIME:~6,2%
 set ms=%TIME:~9,2%
 set CURTIME=%h%_%m%_%s%
 set PGUSER=postgres
-set PGPASSWORD=123
-C:/PostgreSQL/9.4/bin\pg_dump.exe --host localhost --port 5432 --username "postgres" --no-password  --format custom --compress 5 --inserts --column-inserts  --verbose --quote-all-identifiers --file ".\BACKUP\%CURDATE%_%CURTIME%_MIG_BACKUP.sql" --schema "cmodb" "CMO 2017"
+C:/PostgreSQL/9.4/bin\pg_dump.exe --host localhost --port 5432 --username "postgres" --no-password  --format custom --compress 5 --encoding UTF8 --inserts --column-inserts  --verbose --quote-all-identifiers --file ".\BACKUP\%CURDATE%_%CURTIME%_MIG_BACKUP.sql" --schema "cmodb" "CMO 2017"
 set CURDATE=
 set CURTIME=
 set PGPASSWORD=
